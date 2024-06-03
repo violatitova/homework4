0f5227d76792ae95b3ba7dd1a41b409a995b82b2
violatitova <vasya_jewelry@mail.ru>
2024-06-03 17:42:20 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,12 @@
+my_string = input('какой сейчас месяц:' )
+print(my_string)
+print(len(my_string))
+print(my_string .upper())
+print(my_string .lower())
+print('сейчас первый месяц лета - июнь' .replace(__old=' ' , __new=''))
+print(my_string[0])
+print(my_string[-1])
+
+
+
+
